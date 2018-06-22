013a9eb6260d40ea6a4d796579ab075553d05f78
Gaurav Sharma <dev.gaurav94@gmail.com>
2018-06-23 00:19:38 +0530

    TwoKeysKeyboard

---
+++ src/leetcode/dynamicprogramming/TwoKeysKeyboard.java
@@ -0,0 +1,37 @@
+package leetcode.dynamicprogramming;
+
+/**
+ * Initially on a notepad only one character 'A' is present. You can perform two operations on this notepad for each step:
+ * <p>
+ * Copy All: You can copy all the characters present on the notepad (partial copy is not allowed).
+ * Paste: You can paste the characters which are copied last time.
+ * Given a number n. You have to get exactly n 'A' on the notepad by performing the minimum number of steps permitted. Output the minimum number of steps to get n 'A'.
+ */
+
+public class TwoKeysKeyboard {
+
+    public int minSteps(int n) {
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i;
+            for (int j = i - 1; j > 1; j--) {
+                if (i % j == 0) {
+                    dp[i] = dp[j] + (i / j);
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new TwoKeysKeyboard().minSteps(6));
+    }
+
+
+}
